3a24f0802e1069f2a94f4c3867014351918e363e
Exon0t <101366576+Exon0t@users.noreply.github.com>
2022-04-07 01:44:16 -0600

    add name list and total voters

--- PyPoll.py
+++ PyPoll.py
@@ -1,24 +1,28 @@
 #Grab the Data from the ellection_results.csv
 import csv
 import os
+
+totalvotes = 0
+candidates = [ ]
+candidatevotes = { }
 # Assign a variable for the file to load and the path.
 file_to_load = os.path.join("Resources", "election_results.csv")
 file_to_save = os.path.join("analysis", "election_analysis.txt")
-outfile = open(file_to_save, "w")
+#outfile = open(file_to_save, "w")
 #read and analyse the data here
 with open(file_to_load) as election_data:
      file_reader = csv.reader(election_data)
      next(file_reader)
      for row in file_reader:
-        print(row[0])
-#Write down the names of each candidate
-outfile.write("Hello World")
-outfile.write("Denver\n")
-outfile.write("Denver\n")
-outfile.write("Denver\n")
-outfile.write("Denver\n")
+        totalvotes= totalvotes+ 1
+        if row[2] not in candidates :
+                candidates.append(row[2]) 
+                print(candidates)
 
-outfile.close()
+print(totalvotes)
+
+#Write down the names of each candidate
+
 #Get the total Votes for each candidate
 
 #Get the total votes cast for the election
